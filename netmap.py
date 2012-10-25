316cab7f0d36c2ab8fa96fcf67399880b257152a
Samuel <samuel@owee.de>
2012-10-26 00:42:08 +0200

    -

--- netmap.py
+++ netmap.py
@@ -10,13 +10,15 @@ https://github.com/leonidg/Poor-Man-s-traceroute
 """
 
 import sys
+import socket
+import netifaces
 from netaddr import IPNetwork
 from netaddr.core import AddrFormatError
 from threading import Thread
 from Queue import Queue
 
 
-num_threads = 1
+MAX_WORKERS = 1
 
 
 class Netmap():
@@ -25,7 +27,16 @@ class Netmap():
         self.networks = []
         self.taskqueue = Queue()
         self.workers = []
-        self.hosts = []
+        self.hosts = {}
+        self.local_addresses = self.localAddresses()
+
+    def localAddresses(self):
+        result = []
+        for i in netifaces.interfaces():
+            addr = netifaces.ifaddresses(i)
+            if 2 in addr:
+                result.append(addr[2][0]['addr'])
+        return result
 
     def addNetwork(self, network):
         try:
@@ -40,9 +51,9 @@ class Netmap():
         # build up queue
         for network in self.networks:
             for ip in network.iter_hosts():
-                self.taskqueue.put((self.isAlive, str(ip)))
+                self.taskqueue.put((self.addIfAlive, str(ip), network))
         # launching workers
-        for i in range(num_threads):
+        for i in range(MAX_WORKERS):
             w = Thread(target=self.worker)
             w.daemon = True
             self.workers.append(w)
@@ -51,26 +62,78 @@ class Netmap():
         self.taskqueue.join()
 
     def worker(self):
-        # while not self.taskqueue.empty():
         while True:
             task = self.taskqueue.get()
-            task[0](task[1])
+            task[0](task[1], task[2])
             self.taskqueue.task_done()
 
-    def getDot(self):
-        pass
+    def addIfAlive(self, ip, network):
+        #print "Ping %s" % ip
+        if self.icmp(ip) is not None:
+            if ip not in self.hosts:
+                self.hosts[ip] = {'net': network, 'next': []}
+                self.taskqueue.put((self.addTraced, ip, network))
 
-    def isAlive(self, ip):
-        print ip
+    def addTraced(self, ip, network):
+        #print "Trace %s" % ip
+        trace = self.trace(ip)
+        for hop, nexthop in zip(trace[::1], trace[1::1]):
+            if hop not in self.hosts:
+                self.hosts[hop] = {'next': []}
+            if nexthop not in self.hosts:
+                self.hosts[nexthop] = {'next': []}
+            if nexthop not in self.hosts[hop]['next']:
+                self.hosts[hop]['next'].append(nexthop)
 
+    def icmp(self, ip, ttl=30):
+        icmp = socket.getprotobyname('icmp')
+        udp = socket.getprotobyname('udp')
+        recv_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+        send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
+        send_sock.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+        recv_sock.settimeout(0.2)
+        recv_sock.bind(('', 0))
+        send_sock.sendto('', (ip, 1))  # port 1 is fine. doen't matter with icmp?
+        addr = None
+        try:
+            _, addr = recv_sock.recvfrom(512)
+            addr = addr[0]
+        except socket.error:
+            pass
+        finally:
+            send_sock.close()
+            recv_sock.close()
+        if ip in self.local_addresses or addr not in self.local_addresses:
+            return addr
+        else:
+            return None
 
-class Traceroute():
-    pass
+    def trace(self, ip):
+        result = []
+        hop = None
+        ttl = 1
+        while hop != ip and ttl <= 30:
+            hop = self.icmp(ip, ttl)
+            if hop is not None:
+                result.append(hop)
+            ttl += 1
+        return result
 
+    def printDot(self):
+        print "graph G {"
+        for host in self.hosts:
+            if 'net' in self.hosts[host]:
+                #print "node [share=ellipse];"
+                print "\"%s\" -- \"%s\";" % (self.hosts[host]['net'], host)
+            if 'next' in self.hosts[host]:
+                #print "node [share=box];"
+                for nexthop in self.hosts[host]['next']:
+                    print "\"%s\" -- \"%s\";" % (host, nexthop)
+        print "}"
 
 if __name__ == '__main__':
     app = Netmap()
     for arg in sys.argv[1:]:
         app.addNetwork(arg)
     app.scan()
-    print app.getDot()
+    app.printDot()
